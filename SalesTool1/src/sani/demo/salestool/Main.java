934192e68ccea19d17ff1deb7f1dd02855b103bd
Battur Sainbayar <saniood@yahoo.com>
2016-10-02 14:33:35 +0300

    HI test1

---
+++ SalesTool1/src/sani/demo/salestool/Main.java
@@ -0,0 +1,21 @@
+package sani.demo.salestool;
+
+class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		
+		data.display();
+		displayGreeting();
+		
+		
+		}   
+	private static void displayGreeting()  {
+		
+		System.out.println("Hello happy sales people");
+		System.out.println("This app shows how sales data");
+		System.out.println("This is test2");
+	}
+
+}
